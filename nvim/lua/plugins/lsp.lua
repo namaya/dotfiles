d38d44535406f8a4490ed6548abce25465ebe25d
namaya <n3amaya@gmail.com>
2024-10-01 22:53:44 -0500

    add gopls language server

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,32 +1,54 @@
-return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      --
-      -- Configuration files to hook into the Neovim LSP client.
-      --
-      -- For more information, see https://github.com/neovim/nvim-lspconfig .
-      --
-      { "neovim/nvim-lspconfig" },
-      --
-      -- Neovim package manager for LSP servers, linters, and formatters.
-      --
-      -- For more information, see  https://github.com/williamboman/mason.nvim .
-      --
-      { "williamboman/mason.nvim" }
-    },
-    opts = {
-      ensure_installed = {
-        "gopls",
-        "pyright",
-        "lua_ls"
-      }
-    },
-    config = function(_, opts)
-      require("mason").setup {}
-      require("mason-lspconfig").setup(opts)
 
-      local lspconfig = require "lspconfig"
+return {
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            --
+            -- Configuration files to hook into the Neovim LSP client.
+            --
+            -- For more information, see https://github.com/neovim/nvim-lspconfig .
+            --
+            { "neovim/nvim-lspconfig" },
+            --
+            -- Neovim package manager for LSP servers, linters, and formatters.
+            --
+            -- For more information, see  https://github.com/williamboman/mason.nvim .
+            --
+            { "williamboman/mason.nvim" }
+        },
+        config = function()
+            require("mason").setup {}
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls", "pyright", "gopls" }
+            }
+
+            local lspconfig = require "lspconfig"
+
+            -- Golang
+            lspconfig.gopls.setup {}
+            
+            vim.api.create_nvim_autocmd("BufWritePre", {
+              pattern = "*.go",
+              callback = function()
+                local params = vim.lsp.util.make_range_params()
+                params.context = {only = {"source.organizeImports"}}
+                -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+                -- machine and codebase, you may want longer. Add an additional
+                -- argument after params if you find that you have to write the file
+                -- twice for changes to be saved.
+                -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+                local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+                for cid, res in pairs(result or {}) do
+                  for _, r in pairs(res.result or {}) do
+                    if r.edit then
+                      local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                      vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                    end
+                  end
+                end
+                vim.lsp.buf.format({async = false})
+              end
+            })
 
       lspconfig.gopls.setup {}
       lspconfig.pyright.setup {}
